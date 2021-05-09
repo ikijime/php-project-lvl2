7899ae1a9558d4fe04743d109fd48c4b97ba2ba4
Alexei Utkin <lith@protonmail.ch>
2021-05-09 23:59:52 +1000

    Update Differ

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -14,20 +14,20 @@ class GenDiffTest extends TestCase
     {
         $this->expectedFlatOutput = file_get_contents(__DIR__ . '/fixtures/expectedFlatStylish');
     }
-
-    public function testFlatStylishFormattingJson(): void
+    
+    public function test_flat_stylish_formatting_json(): void
     {
         $output = genDiff(__DIR__ . '/fixtures/file1.json', __DIR__ . '/fixtures/file2.json');
         $this->assertEquals($this->expectedFlatOutput, $output);
     }
-
-    public function testFlatStylishFormattingYaml(): void
+    
+    public function test_flat_stylish_formatting_yaml(): void
     {
         $output = genDiff(__DIR__ . '/fixtures/file1.yml', __DIR__ . '/fixtures/file2.yaml');
         $this->assertEquals($this->expectedFlatOutput, $output);
     }
 
-    public function testFlatStylishFormattingMixed(): void
+    public function test_flat_stylish_formatting_mixed(): void
     {
         $output = genDiff(__DIR__ . '/fixtures/file1.json', __DIR__ . '/fixtures/file2.yaml');
         $this->assertEquals($this->expectedFlatOutput, $output);
